9d956d01568bb43d1ab5057de328358b434c678a
Khalid Hajinur <khalid.hajinur@stscommercial.co.uk>
2018-07-16 16:07:22 +0100

    first commit

---
+++ src/javatrainingtask1/TrainingProject.java
@@ -0,0 +1,10 @@
+package javatrainingtask1;
+
+public class TrainingProject {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author khalidhaji
+ *
+ */
+module javatrainingtask1 {
+}
